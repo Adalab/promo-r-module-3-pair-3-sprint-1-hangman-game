ad94704a236811d69c0a55d5be2f57f2db5a3630
Marcela B Petroli <marcelabpetroli@gmail.com>
2022-11-23 11:20:21 +0100

    Add function of control input

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,81 @@
 import '../styles/App.scss';
-import {useState} from 'react';
+import { useState } from 'react';
 
 function App() {
-  let [numberOfErrors, setNumberOfErrors] = useState(0);
+  const [numberOfErrors, setNumberOfErrors] = useState(0);
+  const [lastLetter, setLastLetter] = useState('');
 
   const handleClick = () => {
-    setNumberOfErrors(numberOfErrors+=1);
-  }; 
+    setNumberOfErrors(numberOfErrors + 1);
+  };
+
+  const handleInput = (ev) => {
+    setLastLetter(ev.target.value);
+  };
 
   return (
-    <div className="page">
+    <div className='page'>
       <header>
-        <h1 className="header__title">Juego del ahorcado</h1>
+        <h1 className='header__title'>Juego del ahorcado</h1>
       </header>
-      <main className="main">
+      <main className='main'>
         <section>
-          <div className="solution">
-            <h2 className="title">Solución:</h2>
-            <ul className="letters">
-              <li className="letter">k</li>
-              <li className="letter">a</li>
-              <li className="letter"></li>
-              <li className="letter">a</li>
-              <li className="letter">k</li>
-              <li className="letter">r</li>
-              <li className="letter"></li>
-              <li className="letter">k</li>
-              <li className="letter">e</li>
-              <li className="letter">r</li>
+          <div className='solution'>
+            <h2 className='title'>Solución:</h2>
+            <ul className='letters'>
+              <li className='letter'>k</li>
+              <li className='letter'>a</li>
+              <li className='letter'></li>
+              <li className='letter'>a</li>
+              <li className='letter'>k</li>
+              <li className='letter'>r</li>
+              <li className='letter'></li>
+              <li className='letter'>k</li>
+              <li className='letter'>e</li>
+              <li className='letter'>r</li>
             </ul>
           </div>
-          <div className="error">
-            <h2 className="title">Letras falladas:</h2>
-            <ul className="letters">
-              <li className="letter">f</li>
-              <li className="letter">q</li>
-              <li className="letter">h</li>
-              <li className="letter">p</li>
-              <li className="letter">x</li>
+          <div className='error'>
+            <h2 className='title'>Letras falladas:</h2>
+            <ul className='letters'>
+              <li className='letter'>f</li>
+              <li className='letter'>q</li>
+              <li className='letter'>h</li>
+              <li className='letter'>p</li>
+              <li className='letter'>x</li>
             </ul>
           </div>
-          <form className="form">
-            <label className="title" htmlFor="last-letter">
+          <form className='form'>
+            <label className='title' htmlFor='last-letter'>
               Escribe una letra:
             </label>
-            <input autoComplete="off" className="form__input" maxLength="1" type="text" name="last-letter" id="last-letter" />
+            <input
+              autoComplete='off'
+              className='form__input'
+              maxLength='1'
+              type='text'
+              name='last-letter'
+              id='last-letter'
+              value={lastLetter}
+              onChange={handleInput}
+            />
           </form>
         </section>
         <button onClick={handleClick}>Incrementar</button>
         <section className={`dummy error-${numberOfErrors}`}>
-          <span className="error-13 eye"></span>
-          <span className="error-12 eye"></span>
-          <span className="error-11 line"></span>
-          <span className="error-10 line"></span>
-          <span className="error-9 line"></span>
-          <span className="error-8 line"></span>
-          <span className="error-7 line"></span>
-          <span className="error-6 head"></span>
-          <span className="error-5 line"></span>
-          <span className="error-4 line"></span>
-          <span className="error-3 line"></span>
-          <span className="error-2 line"></span>
-          <span className="error-1 line"></span>
+          <span className='error-13 eye'></span>
+          <span className='error-12 eye'></span>
+          <span className='error-11 line'></span>
+          <span className='error-10 line'></span>
+          <span className='error-9 line'></span>
+          <span className='error-8 line'></span>
+          <span className='error-7 line'></span>
+          <span className='error-6 head'></span>
+          <span className='error-5 line'></span>
+          <span className='error-4 line'></span>
+          <span className='error-3 line'></span>
+          <span className='error-2 line'></span>
+          <span className='error-1 line'></span>
         </section>
       </main>
     </div>
